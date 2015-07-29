b0073b5ca9699a70bef01967bba47d44066d8db3
Frank W. Rahn <frank@frank-rahn.de>
2015-07-29 23:15:35 +0200

    Neues Packages

---
+++ securities-management/src/main/java/de/rahn/finances/entities/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright © 2015 by Frank W. Rahn. Alle Rechte vorbehalten. All rights reserved.
+ */
+
+/**
+ * Die Datenbankrepräsentationen der Wertpapierverwaltung.
+ * @author Frank W. Rahn
+ */
+package de.rahn.finances.entities;
