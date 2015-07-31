86dc61ba5ab220f3fa1a0fe768971fd9917e3a28
Frank W. Rahn <frank@frank-rahn.de>
2015-07-31 23:02:55 +0200

    ComponentScan hinzugefügt

--- securities-management/src/main/java/de/rahn/finances/SecuritiesManagementApplication.java
+++ securities-management/src/main/java/de/rahn/finances/SecuritiesManagementApplication.java
@@ -1,16 +1,18 @@
 /*
  * Copyright © 2015 by Frank W. Rahn. Alle Rechte vorbehalten. All rights reserved.
  */
-package de.rahn.finances.server;
+package de.rahn.finances;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Die Startklasse für diesen Server.
  * @author Frank W. Rahn
  */
 @SpringBootApplication
+@ComponentScan
 public class SecuritiesManagementApplication {
 
 	/**
