9981b9b32aa85fb859851543948733b3978ae19a
Frank W. Rahn <frank@frank-rahn.de>
2015-07-31 23:02:21 +0200

    Wegen des ComponenScan, JPA-Unterstützung, etc. in die Root des Projektes verschoben

--- securities-management/src/main/java/de/rahn/finances/server/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright © 2015 by Frank W. Rahn. Alle Rechte vorbehalten. All rights reserved.
- */
-
-/**
- * In diesem Package liegen die Klassen für den Server.
- * @author Frank W. Rahn
- */
-package de.rahn.finances.server;
