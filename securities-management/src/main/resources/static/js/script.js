5e7f7c08236372fb7a82d6b807b5f2e1b3720da9
Frank W. Rahn <frank@frank-rahn.de>
2015-09-02 23:37:05 +0200

    Dialog hinzugefügt

--- securities-management/src/main/resources/static/js/script.js
+++ securities-management/src/main/resources/static/js/script.js
@@ -1,5 +1,27 @@
 function alertMessage(stat, text) {
-	$("#placeholder").html('<div class="alert alert-'+stat+' fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' + text + '</div>').alert();
+	$("#placeholder").html('<div class="alert alert-'+stat+' fade in" role="alert"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' + text + '</div>').alert();
+}
+
+function modalMessage(titel, text) {
+	$("#placeholder").html(
+`<div class="modal fade" id="modal" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Titel</h4>
+			</div>
+			<div class="modal-body">
+				<p>Text</p>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Abbrechen</button>
+			</div>
+		</div>
+	</div>
+</div>`
+	);
+	$("#modal").modal();
 }
 
 $("#delete").click(function (event) {
