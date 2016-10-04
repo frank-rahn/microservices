8f98a541177b70cf644ad937042b9cf50143bf90
Frank W. Rahn <frank@frank-rahn.de>
2016-10-04 22:56:30 +0200

    Weitere Test mit Parameterized und Spring Context

---
+++ securities-management/domains/src/test/java/de/rahn/finances/domains/repositories/SecuritiesRepositoryParameterizedTest.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2011-2016 Frank W. Rahn and the project authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.rahn.finances.domains.repositories;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.MethodRule;
+import org.junit.rules.TestRule;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
+
+import de.rahn.finances.domains.config.DomainsConfiguration;
+import de.rahn.finances.domains.entities.Security;
+
+/**
+ * Tests für {@link SecuritiesRepository}
+ *
+ * @author Frank W. Rahn
+ */
+@RunWith(Parameterized.class)
+@SpringBootTest(classes = DomainsConfiguration.class)
+@DataJpaTest
+public class SecuritiesRepositoryParameterizedTest {
+
+	/**
+	 * @return die Liste der Test Fixtures
+	 */
+	@Parameters(name = "find({1}) = WKN({0})")
+	public static Iterable<Object[]> parameters() {
+		List<Object[]> parameters = new ArrayList<>();
+
+		for (int i = 1; i < 10; i++) {
+			parameters.add(new Object[] { "10000" + i, "067e6162-3b6f-4ae2-a171-2470b63df00" + i });
+		}
+
+		return parameters;
+	}
+
+	/** Aktiviere die Spring {@link TestRule}s. */
+	@ClassRule
+	public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+
+	/** Aktiviere die Spring {@link MethodRule}s. */
+	@Rule
+	public final SpringMethodRule springMethodRule = new SpringMethodRule();
+
+	/** Die zu testende Klasse. */
+	@Autowired
+	private SecuritiesRepository repository;
+
+	/** Test Fixture: WKN des Wertpapiers */
+	@Parameter(0)
+	public String wkn;
+
+	/** Test Fixture: ID des Wertpapiers */
+	@Parameter(1)
+	public String id;
+
+	/**
+	 * Test method for {@link SecuritiesRepository#findOne(String).
+	 */
+	@Test
+	public void testFindOne() {
+		Security security = repository.findOne(id);
+
+		assertThat(security).isNotNull();
+		assertThat(security.getId()).isEqualTo(id);
+		assertThat(security.getWkn()).isEqualTo(wkn);
+	}
+
+}
