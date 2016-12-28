c4752e0df26d962c17fff8bd7131269c87bcca6c
Frank W. Rahn <frank@frank-rahn.de>
2016-12-28 19:37:44 +0100

    Typo

--- securities-management/server-web/src/main/java/de/rahn/finances/server/web/ui/WorkaroundErrorController.java
+++ securities-management/server-web/src/main/java/de/rahn/finances/server/web/ui/WorkaroundErrorController.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011-2016 Frank W. Rahn and the project authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.rahn.finances.server.web.ui;
 
 import static org.springframework.boot.autoconfigure.web.ErrorProperties.IncludeStacktrace.ALWAYS;
@@ -11,10 +26,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Workaround für <a href="https://github.com/spring-projects/spring-boot/issues/5638">Bug 5638</a>. Durch diesen Bug wird auf
- * der Error-Seite kein
- * Security Context zu Verfügung gestellt. Angeblich nur wenn spring-boot-actuator im Klassenpfad ist ...
+ * der Error-Seite kein Security Context zu Verfügung gestellt. Angeblich nur wenn spring-boot-actuator im Klassenpfad ist ...
  *
- * @author hv11h36
+ * @author Frank W. Rahn
  */
 @Controller
 public class WorkaroundErrorController extends BasicErrorController {
