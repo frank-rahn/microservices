252ce9a699674c077004c091f8f4f28ea6f38893
Frank W. Rahn <frank@frank-rahn.de>
2018-02-17 17:25:04 +0100

    Runner verschoben

--- securities-management/server-web/src/test/java/de/rahn/finances/server/web/ui/SecuritiesController2Test.java
+++ securities-management/server-web/src/test/java/de/rahn/finances/server/web/ui/SecuritiesController2Test.java
@@ -20,7 +20,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import de.rahn.finances.domains.entities.Security;
 import de.rahn.finances.services.SecuritiesService;
